42518850a22d731cf3501829004eea18ffbf4d4b
raffaele <raffaeleventriglia6@gmail.com>
2022-10-04 19:35:38 +0200

    minor update

--- BankAccount/src/App.java
+++ BankAccount/src/App.java
@@ -6,23 +6,23 @@ public class App {
 
         System.out.println("Account Number: " + account.getAccountNumber());
         System.out.println("Saldo disponibile: " + account.getBalance());
-        account.deposit(1245.50);
+        account.deposit(145.50);
         System.out.println("Saldo disponibile: " + account.getBalance());
-        account.withdraw(125.50);
+        account.withdraw(25.50);
         System.out.println("Saldo disponibile: " + account.getBalance());
 
         System.out.println("Account Number: " + account2.getAccountNumber());
         System.out.println("Saldo disponibile: " + account2.getBalance());
-        account2.deposit(1245.50);
+        account2.deposit(4245.50);
         System.out.println("Saldo disponibile: " + account2.getBalance());
-        account2.withdraw(125.50);
+        account2.withdraw(12.50);
         System.out.println("Saldo disponibile: " + account2.getBalance());
 
         System.out.println("Account Number: " + account3.getAccountNumber());
         System.out.println("Saldo disponibile: " + account3.getBalance());
-        account3.deposit(1245.50);
+        account3.deposit(245.50);
         System.out.println("Saldo disponibile: " + account3.getBalance());
-        account3.withdraw(125.50);
+        account3.withdraw(155.50);
         System.out.println("Saldo disponibile: " + account3.getBalance());
     }
 }
