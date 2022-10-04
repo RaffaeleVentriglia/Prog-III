626698f57d0acdb96f674b533b1d63d00b35fb33
raffaele <raffaeleventriglia6@gmail.com>
2022-10-04 10:31:21 +0200

    purse

--- Purse/src/App.java
+++ Purse/src/App.java
@@ -1,9 +1,21 @@
+import javax.swing.JOptionPane;
+import java.io.IOException;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Purse p1 = new Purse();
-        p1.addNickel(5);
-        p1.addDimes(10);
-        p1.addQuarter(25);
+        int count;
+        String input;
+
+        input = JOptionPane.showInputDialog("Inserire nickel");
+        count = Integer.parseInt(input);
+        p1.addNickel(count);
+        input = JOptionPane.showInputDialog("Inserire Dime");
+        count = Integer.parseInt(input);
+        p1.addDimes(count);
+        input = JOptionPane.showInputDialog("Inserire quarter");
+        count = Integer.parseInt(input);
+        p1.addQuarter(count);
         System.out.println("Totale: £" + p1.getTotal());
     }
 }
